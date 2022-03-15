57775e198875094f893132968b278d7066d2adcf
Himmat Singh Khalsa <91692999+thehimmat@users.noreply.github.com>
2022-03-15 12:54:12 -0600

    Time: 92 ms (53.56%), Space: 43.3 MB (14.40%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let p1 = 0;
+    let p2 = 1;  
+    while (p1 < numbers.length || p2 <= numbers.length) {
+        if (numbers[p1] + numbers[p2] === target) {
+            return [p1 + 1, p2 + 1];
+        } else if (p2 === numbers.length || numbers[p1] + numbers[p2] > target || numbers[p1] === numbers[p1 + 1]) {
+            p1++;
+            p2 = p1 + 1;
+        } else {
+            p2++;
+        }
+    }
+    // for (let i = 0; i < numbers.length; i++) {
+    //     for (let j = i + 1; j < numbers.length; j++) {
+    //         if (numbers[i] + numbers[j] === target) {
+    //             return [i + 1, j + 1];
+    //         }
+    //     }
+    // }
+};
