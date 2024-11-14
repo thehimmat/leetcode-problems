d179a039e1c90eb9eb64dd693e0da22e7a99b743
Himmat Singh Khalsa <91692999+thehimmat@users.noreply.github.com>
2024-11-14 12:56:24 -0800

    Time: 1 ms (83.35%), Space: 52.8 MB (50.33%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let beg = 0, end = x < 16 ? x : Math.floor(x/4), ans = 0
-    while (beg <= end) {
-        const mid = Math.floor((beg+end)/2)
-        if (mid * mid > x) {
-            end = mid - 1
-        } else {
-            ans = mid
-            beg = mid + 1
-        }
+    const lookup = [0,1,1,1,2,2,2,2,2,3,3,3,3,3,3,3,4]
+    if (x <= 16) return lookup[x]
+
+    let guess = x
+    while (guess * guess > x) {
+        guess = Math.floor((guess + Math.floor(x / guess)) / 2);
     }
-    return ans
+    return guess
 };
