d46aad0ce5b458a8975ed83fe7815b96dc5b5932
Himmat Singh Khalsa <91692999+thehimmat@users.noreply.github.com>
2025-02-06 21:27:50 -0800

    Time: 5 ms (2.81%), Space: 48.8 MB (52.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,10 +2,9 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n, memo = {}) {
-    if (n <= 2) return n // base cases
-    if (memo[n]) return memo[n] // check if solution already found
-
-    memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo) // store previous result 
-    return memo[n] // exit recursion or return final solution
+var climbStairs = function(n) {
+    if (n <= 3) { 
+        return n
+    }
+    return 2 * climbStairs(n - 2) + climbStairs(n - 3);
 };
