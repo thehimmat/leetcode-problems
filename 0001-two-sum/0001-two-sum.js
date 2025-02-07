63fcafa37c98524fd0754f1799aa3803a91d39fc
Himmat Singh Khalsa <91692999+thehimmat@users.noreply.github.com>
2025-02-06 20:58:58 -0800

    Time: 49 ms (29.61%), Space: 49.9 MB (62.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,11 +5,11 @@
  */
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            if (i === j) j++
+        for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 return [i, j]
             }
         }
     }
+    return []
 };
