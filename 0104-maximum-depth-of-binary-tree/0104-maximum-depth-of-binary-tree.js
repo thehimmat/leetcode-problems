ddb32804c73a79748780a0de5c9110b6bc99c892
Himmat Singh Khalsa <91692999+thehimmat@users.noreply.github.com>
2025-02-18 21:23:32 -0800

    Time: 0 ms (100%), Space: 57 MB (5.3%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -10,14 +10,7 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const maxDepth = (root) => {
-    if (!root) return 0;
-    return findDepth(root, 0);
+var maxDepth = function(root) {
+    if (!root) return 0
+    return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1
 };
-
-const findDepth = (root, level) => {
-    if (!root) return level;
-    let leftDepth = findDepth(root.left, level+1)
-    let rightDepth = findDepth(root.right, level+1)
-    return Math.max(leftDepth, rightDepth)
-}
