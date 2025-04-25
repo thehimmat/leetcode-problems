10bc6d944193790686222aa458e122eb437443e6
Himmat Singh Khalsa <91692999+thehimmat@users.noreply.github.com>
2025-04-25 15:49:32 -0700

    Time: 0 ms (100%), Space: 12.6 MB (21.63%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        len1, len2 = len(str1), len(str2)
+
+        def isDivisor(l):
+            if len1 % l or len2 % l:
+                return False
+            f1, f2 = len1 // l, len2 // l
+            return str1[:l] * f1 == str1 and str1[:l] * f2 == str2
+
+        for l in range(min(len1, len2), 0, -1):
+            if isDivisor(l):
+                return str1[:l]
+        return ""
