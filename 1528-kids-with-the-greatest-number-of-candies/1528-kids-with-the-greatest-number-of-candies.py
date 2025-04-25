f55492a1f341fd302c9d3834eee2ee6a6e9b8f2f
Himmat Singh Khalsa <91692999+thehimmat@users.noreply.github.com>
2025-04-25 16:05:42 -0700

    Time: 0 ms (100%), Space: 12.5 MB (53.02%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        maxC = max(candies)
+
+        return [(cur + extraCandies >= maxC) for cur in candies]
